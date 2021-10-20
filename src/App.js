1176bd29f5979df15e0b6e68ee2b990d3d80d573
Anastasia <nstsrhtn@gmail.com>
2021-10-20 18:55:17 +0700

    2 days

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import './styles/App.css'
 import PostList from "./Components/PostList";
-import MyInput from "./Components/UI/input/MyInput";
-import MyButton from "./Components/UI/button/MyButton";
 import PostForm from "./Components/PostForm";
+import MySelect from "./Components/UI/select/MySelect";
+import MyInput from "./Components/UI/input/MyInput";
+import PostFilter from "./Components/PostFilter";
+import MyModal from "./Components/UI/MyModal/MyModal";
+import MyButton from "./Components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePost";
+import axios from "axios";
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title:'Javascript', body:'Description'},
-        {id: 2, title:'Javascript 2', body:'Description'},
-        {id: 3, title:'Javascript 3', body:'Description'}])
+        {id: 1, title:'Javascript', body:'n'},
+        {id: 2, title:'avascript 2', body:'iption'},
+        {id: 3, title:'vascript 3', body:'Description'}])
+
+    const[filter, setFilter] = useState({sort:'', query: ''})
+    const [modal, setModal] = useState(false)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+     async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+         setPosts(response.data)
+    }
 
     const createPost = (newPost) =>{
         setPosts([...posts, newPost])
+        setModal(false)
     }
-    const removePost =(post) => {
+
+    const removePost = (post) => {
         setPosts(posts.filter(p => p.id !==post.id))
     }
 
+
   return (
     <div className="App">
-        <PostForm create={createPost}/>
-       <PostList
-           remove = {removePost}
-           posts={posts}
-           title="Список постов 1"/>
+        <button onClick={fetchPosts}> Get posts</button>
+        <MyButton style={{marginTop:30}} onClick={() => setModal(true)}>
+            Создать пользователя
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
+        </MyModal>
+        <hr style={{margin: '15px 0'}}/>
+        <PostFilter
+            filter={filter}
+            setFilter={setFilter}
+        />
+            <PostList
+                remove = {removePost}
+                posts={sortedAndSearchedPosts}
+                title="Список постов 1"/>
+
     </div>
   );
 }
--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({posts, title, remove}) => {
+    if (!posts.length) {
+        return (
+            <h1 style={{textAlign: 'center'}}>
+                Посты не найдены!
+            </h1>
+        )
+    }
     return (
         <div>
             <h1 style={{textAlign:'center'}}>
                 {title}
             </h1>
-            {posts.map((post, index) =>
-             <PostItem number={index + 1}
-                       post={post}
-                       key={post.id}
-                       remove={remove}/>
-            )}
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        className='post'
+                        >
+                    <PostItem number={index + 1}
+                              post={post}
+
+                              remove={remove}/>
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
+
         </div>
     );
 };
